df0a605f3f73c01303d25efac6d22c3caf488d65
Catotron <cam29706@gmail.com>
2023-09-12 14:29:47 +1000

    Improved Terminal Output [v0.5 | Experimental-5]

--- Main.py
+++ Main.py
@@ -26,6 +26,31 @@ CBOLD = '\033[1m'
 intents = nextcord.Intents.all()
 bot = commands.Bot()
 
+### Terminal Functions
+def formatOutput(output, status):
+    current_time = datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S.%f')[:-3]
+    if status == "Normal": print("| "+str(current_time)+" || "+output)
+    elif status == "Good": print(CGREEN +"| "+str(current_time)+" || "+output+ CLEAR)
+    elif status == "Error": print(CRED +"| "+str(current_time)+" || "+output+ CLEAR)
+    elif status == "Warning": print(CYELLOW +"| "+str(current_time)+" || "+output+ CLEAR)
+
+### Save Functions
+async def save(command, userID):
+    updateCommandUsage(command, userID)
+    updateCommandsUsed(command)
+    updateCommandsSent(userID)
+
+def updateCommandUsage(command, userID):
+    formatOutput(output="/"+command+" Used by ("+str(userID)+")", status="Normal")
+    try:
+        # save data
+        formatOutput(output="    Successfully Saved", status="Good")
+    except Exception as e: formatOutput(output="        Error occured while saving: "+str(e), status="Error")
+
+def updateCommandsUsed(command): pass
+
+def updateCommandsSent(userID): pass
+
 ### Startup
 error = False
 start_time = datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')
@@ -54,28 +79,42 @@ else:
     async def on_ready():
         end_time = datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')
         startup_time = datetime.datetime.strptime(end_time,'%d-%m-%Y %H:%M:%S') - datetime.datetime.strptime(start_time,'%d-%m-%Y %H:%M:%S')
-        print(CGREEN + f"-----------------------------------------\n| Logged on as {bot.user}\n| Running Version "+version+"\n-----------------------------------------\nTime Taken: "+str(startup_time)+ CLEAR)
-        print("Time Taken: "+str(startup_time))
+        print(CGREEN + f"-----------------------------------------\n| Logged on as {bot.user}\n| Running Version "+version+"\n| Time Taken: "+str(startup_time)+"\n-----------------------------------------"+ CLEAR)
 
 ### Slash Commands
-@bot.slash_command(guild_ids=guild_ID, name="shutdown", description="Turns off the bot")
+@bot.slash_command(guild_ids=[guild_ID], name="shutdown", description="Turns off the bot")
 async def testCommand(interaction: nextcord.Interaction):
     await interaction.send("Shutting Down Bot!")
+    command = 'shutdown'
+    userID = interaction.user.id
+    await save(command, userID)
 
-@bot.slash_command(guild_ids=guild_ID, name="bot_stats", description="Displays stats for the bot")
-async def CommandName(interaction: nextcord.Interaction):
+@bot.slash_command(guild_ids=[guild_ID], name="bot_stats", description="Displays stats for the bot")
+async def botStats(interaction: nextcord.Interaction):
     await interaction.send("Bot Stats")
+    command = 'bot_stats'
+    userID = interaction.user.id
+    await save(command, userID)
 
-@bot.slash_command(guild_ids=guild_ID, name="user_lookup", description="Lookup stats for a user")
-async def CommandName(interaction: nextcord.Interaction):
+@bot.slash_command(guild_ids=[guild_ID], name="user_lookup", description="Lookup stats for a user")
+async def userLookup(interaction: nextcord.Interaction):
     await interaction.send("Stats for (user)")
+    command = 'user_lookup'
+    userID = interaction.user.id
+    await save(command, userID)
 
-@bot.slash_command(guild_ids=guild_ID, name="command_leaderboard", description="Shows leaderboard for each command usage")
-async def CommandName(interaction: nextcord.Interaction):
+@bot.slash_command(guild_ids=[guild_ID], name="command_leaderboard", description="Shows leaderboard for each command usage")
+async def commandLeaderboard(interaction: nextcord.Interaction):
     await interaction.send("Command Leaderboard:")
+    command = 'command_leaderboard'
+    userID = interaction.user.id
+    await save(command, userID)
 
-# @bot.slash_command(guild_ids=guild_ID, name="placeholder", description="placeholder")
+# @bot.slash_command(guild_ids=[guild_ID], name="placeholder", description="placeholder")
 # async def CommandName(interaction: nextcord.Interaction):
 #     await interaction.send("a response")
+#     command = 'command_name'
+#     userID = interaction.user.id
+#     await save(command, userID)
 
 bot.run(bot_token)
